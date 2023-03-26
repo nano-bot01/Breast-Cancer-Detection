6cc791f1eb0ec21f25cdcd07e9dc8ff83f3797af
Ankit Nainwal <78251168+nano-bot01@users.noreply.github.com>
2023-03-27 02:22:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-"""
-Created on Sun Mar 26 22:15:42 2023
+# """
+# Created on Sun Mar 26 22:15:42 2023
 
-@author: ankit
-"""
+# @author: ankit
+# """
+
+# import numpy as np
+# import pandas as pd
+# import pickle
+# from flask import Flask, request, render_template
+
+# app = Flask(__name__)
 
 import numpy as np
 import pandas as pd
@@ -17,32 +24,71 @@ model = pickle.load(open("breast_cancer_detector.pickle", 'rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+# @app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET', 'POST']) 
 def predict():
-    input_features = [float(x) for x in request.form.values()]
-    feature_values = [np.array(input_features)]
+    if request.method == 'POST':
+        input_features = [float(x) for x in request.form.values()]
+        feature_values = [np.array(input_features)]
 
-    feature_names = ['mean radius', 'mean texture', 'mean perimeter', 'mean area',
-                     'mean smoothness', 'mean compactness', 'mean concavity',
-                     'mean concave points', 'mean symmetry', 'mean fractal dimension',
-                     'radius error', 'texture error', 'perimeter error', 'area error',
-                     'smoothness error', 'compactness error', 'concavity error',
-                     'concave points error', 'symmetry error', 'fractal dimension error',
-                     'worst radius', 'worst texture', 'worst perimeter', 'worst area',
-                     'worst smoothness', 'worst compactness', 'worst concavity',
-                     'worst concave points', 'worst symmetry', 'worst fractal dimension',
-                     'target']
+        feature_names = ['mean radius', 'mean texture', 'mean perimeter', 'mean area',
+                         'mean smoothness', 'mean compactness', 'mean concavity',
+                         'mean concave points', 'mean symmetry', 'mean fractal dimension',
+                         'radius error', 'texture error', 'perimeter error', 'area error',
+                         'smoothness error', 'compactness error', 'concavity error',
+                         'concave points error', 'symmetry error', 'fractal dimension error',
+                         'worst radius', 'worst texture', 'worst perimeter', 'worst area',
+                         'worst smoothness', 'worst compactness', 'worst concavity',
+                         'worst concave points', 'worst symmetry', 'worst fractal dimension']
 
-    df = pd.DataFrame(feature_values, columns=feature_names)
+        df = pd.DataFrame(feature_values, columns=feature_names)
 
-    output = model.predict(df)
+        output = model.predict(df)
 
-    if output == 0:
-        result = "---Patient has Breast Cancer---"
-    else:
-        result = "---Patient is Healthy---"
+        if output == 1:
+            result = "---Patient has Breast Cancer---"
+        else:
+            result = "---Patient is Healthy---"
 
-    return render_template('index.html', prediction_text='The Result is : {} '.format(result))
+        return render_template('index.html', prediction_text='The Result is : {} '.format(result))
 
 if __name__ == "__main__":
     app.run()
+
+
+# model = pickle.load(open("breast_cancer_detector.pickle", 'rb'))
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     input_features = [float(x) for x in request.form.values()]
+#     feature_values = [np.array(input_features)]
+
+#     feature_names = ['mean radius', 'mean texture', 'mean perimeter', 'mean area',
+#                      'mean smoothness', 'mean compactness', 'mean concavity',
+#                      'mean concave points', 'mean symmetry', 'mean fractal dimension',
+#                      'radius error', 'texture error', 'perimeter error', 'area error',
+#                      'smoothness error', 'compactness error', 'concavity error',
+#                      'concave points error', 'symmetry error', 'fractal dimension error',
+#                      'worst radius', 'worst texture', 'worst perimeter', 'worst area',
+#                      'worst smoothness', 'worst compactness', 'worst concavity',
+#                      'worst concave points', 'worst symmetry', 'worst fractal dimension']
+
+#     df = pd.DataFrame(feature_values, columns=feature_names)
+
+#     output = model.predict(df)
+
+#     if output == 1:
+#         result = "---Patient has Breast Cancer---"
+#     else:
+#         result = "---Patient is Healthy---"
+
+#     return render_template('index.html', prediction_text='The Result is : {} '.format(result))
+
+# if __name__ == "__main__":
+#     app.run()
+
+
